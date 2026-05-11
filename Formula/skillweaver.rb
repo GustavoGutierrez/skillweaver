b0e4343df9a626229d19641bdcff6d5ede2a5fe2
Gustavo Gutierrez <gustavogutierrezmercado@gmail.com>
2026-05-11 18:26:55 -0500

    chore: update Homebrew formula SHA256 for v0.0.1

--- Formula/skillweaver.rb
+++ Formula/skillweaver.rb
@@ -2,7 +2,7 @@ class Skillweaver < Formula
   desc "TUI package manager for AI agent skills and rules"
   homepage "https://github.com/GustavoGutierrez/skillweaver"
   url "https://github.com/GustavoGutierrez/skillweaver/releases/download/v0.0.1/skillweaver-x86_64-unknown-linux-gnu.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_FIRST_RELEASE"
+  sha256 "6e00a4c086ad30e7cd2b9382c2181759f003351f30d5b8e18b041f11b8057081"
   version "0.0.1"
   license "MIT"
 

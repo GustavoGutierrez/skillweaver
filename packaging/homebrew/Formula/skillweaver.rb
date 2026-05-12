ecfa0be84540d40051d3c7e78e80958c6a0de366
Gustavo Gutierrez <gustavogutierrezmercado@gmail.com>
2026-05-12 09:35:09 -0500

    fix: include v prefix in Homebrew release URL

--- packaging/homebrew/Formula/skillweaver.rb
+++ packaging/homebrew/Formula/skillweaver.rb
@@ -1,7 +1,7 @@
 class Skillweaver < Formula
   desc "TUI package manager for AI agent skills and rules"
   homepage "https://github.com/GustavoGutierrez/skillweaver"
-  url "https://github.com/GustavoGutierrez/skillweaver/releases/download/{{VERSION}}/skillweaver-x86_64-unknown-linux-gnu.tar.gz"
+  url "https://github.com/GustavoGutierrez/skillweaver/releases/download/v{{VERSION}}/skillweaver-x86_64-unknown-linux-gnu.tar.gz"
   sha256 "{{CHECKSUM}}"
   version "{{VERSION}}"
   license "MIT"
